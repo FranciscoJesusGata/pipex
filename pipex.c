4135b8a53a8ad257af0dfbe22a5d120853adaf08
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-06-18 12:56:09 +0200

    Exit status working

--- pipex.c
+++ pipex.c
@@ -6,60 +6,88 @@
 /*   By: fgata-va <fgata-va@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 10:41:58 by fgata-va          #+#    #+#             */
-/*   Updated: 2021/06/16 15:32:05 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/06/18 12:55:23 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
 
+char	**get_path(char **envp)
+{
+	while (ft_strncmp(*envp, "PATH", 4))
+		envp++;
+	return (ft_split(*envp + 5, ':'));
+}
+
+void	free_matrix(char **matrix)
+{
+	int	i;
+
+	i = 0;
+	while (matrix[i])
+	{
+		free(matrix[i]);
+		i++;
+	}
+	free(matrix);
+}
+
+int	ft_executor(char **cmd, char **path, char **envp)
+{
+	int		pid;
+	int		executed;
+	int		i;
+	int		status;
+
+	executed = -1;
+	status = 0;
+	pid = fork();
+	if (pid == 0)
+	{
+		i = 0;
+		while (executed == -1 && path[i])
+		{
+			ft_strlcat(path[i], "/", 30);
+			ft_strlcat(path[i], cmd[0], 30);
+			executed = execve(path[i], cmd, envp);
+			i++;
+		}
+		if (path[i] != NULL)
+			perror(cmd[0]);
+		else
+		{
+			ft_putstr_fd("command not found\n", 1);
+			status = 127;
+		}
+		exit(status);
+	}
+	else
+	{
+		waitpid(pid, &status, WNOHANG);
+		while (status == 0)
+			waitpid(pid, &status, WNOHANG);
+	}
+	return (status / 256);
+}
+
 int	main(int argc, char **argv, char *envp[])
 {
 	int	i;
 	char	**cmd1;
 	char	**path;
-	int		pid;
-	int		executed;
+	int		status;
 
 	i = 0;
-	executed = -1;
+	status = 0;
 	if (argc > 1)
 	{
-		printf("%d, %s\n", argc, *argv);
-		while (ft_strncmp(envp[i], "PATH", 4))
-			i++;
-		path = ft_split(envp[i] + 5, ':');
-		i = 0;
-		while (path[i])
-		{
-			printf("%s ", path[i]);
-			i++;
-		}
-		printf("\n");
+		path = get_path(envp);
 		cmd1 = ft_split(argv[1], ' ');
-		i = 1;
-		while (cmd1[i])
-		{
-			printf("%s ", cmd1[i]);
-			i++;
-		}
-		printf("\n");
-		pid = fork();
-		if (pid != 0)
-		{
-			i = 0;
-			while (executed < 0 && path[i])
-			{
-				ft_strlcat(path[i], "/", 30);
-				ft_strlcat(path[i], cmd1[0], 30);
-				printf("%s %s\n", path[i], cmd1[0]);
-				executed = execve(path[i], cmd1 + 1, envp);
-				i++;
-			}
-			if (executed < 0)
-				perror("execve");
-		}
-		return (0);
+		status = ft_executor(cmd1, path, envp);
+		free_matrix(path);
+		free(cmd1);
 	}
-	return (1);
+	printf("%d\n", status);
+	return (status);
 }
